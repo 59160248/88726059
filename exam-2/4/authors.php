b0313ee3e41c9114c3a164e524615c7b42f21dd0
59160248 <kasemsak456@gmail.com>
2021-03-19 15:59:07 +0700

    ex-3 and ex-4

---
+++ exam-2/4/authors.php
@@ -0,0 +1,19 @@
+<?php
+   echo '{
+      "authors": [
+         { 
+            "authorID": "1111", 
+            "author": "wittawas", 
+            "penname": "witty100" 
+         },{ 
+            "authorID": "1112", 
+            "author": "jakkarin", 
+            "penname": "พ่อบ้านใจกล้า" 
+         },{ 
+            "authorID": "1113", 
+            "author": "worawit", 
+            "penname": "obob" 
+         }
+      ]
+   }';
+?>
